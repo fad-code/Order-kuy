30f6ce04f5d1758f01bf29371a02a3e38fb187eb
fad-code <mfadhilah29@gmail.com>
2025-08-06 14:42:21 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 const data = [
-  { name: 'Pizza', desc: 'pepperoni, mushroom, mozzarella', price: 14, image: '/images/pizza.png' },
+  { name: 'Pizza', desc: 'pepperoni, mushroom, mozzarella', price: 14, image: '/images/Pizza.png' },
   { name: 'Hamburger', desc: 'beef, cheese, lettuce', price: 12, image: '/images/burger.png' },
   { name: 'Beer', desc: 'grain, hops, yeast, water', price: 12, image: '/images/beer.png' },
   { name: 'Hotdog', desc: 'saussage, mustard, pickles', price: 12, image: '/images/hotdog.jpg' },
